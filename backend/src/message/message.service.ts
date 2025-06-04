b353e663617b11e1125d43ad0a320dc9fe30de80
jeremyadasda <jeremy.pulgar@usm.cl>
2025-06-04 02:07:43 +0100

    random message

--- backend/src/message/message.service.ts
+++ backend/src/message/message.service.ts
@@ -11,12 +11,36 @@ interface Message {
 @Injectable()
 export class MessageService {
   // This method simulates fetching a message from a database or external source
-  getHelloMessage(): Message {
-    return {
+  private messages = [
+    {
       id: 1,
-      content: 'Desarrollo de Aplicaciones Web y Sistemas en la Nube',
+      content: '¡Bienvenido a ZEROUNO!',
       timestamp: new Date().toISOString(),
-      source: 'NestJS'
-    };
+      source: 'backend',
+    },
+    {
+      id: 2,
+      content: 'Soluciones informáticas a tu medida.',
+      timestamp: new Date().toISOString(),
+      source: 'backend',
+    },
+    {
+      id: 3,
+      content: 'Desarrollamos tu landing page.',
+      timestamp: new Date().toISOString(),
+      source: 'backend',
+    },
+    {
+      id: 4,
+      content: 'Consultoría en la nube AWS y Google.',
+      timestamp: new Date().toISOString(),
+      source: 'backend',
+    },
+  ];
+  getHelloMessage(): Message {
+    const randomIndex = Math.floor(Math.random() * this.messages.length);
+    // Update timestamp for freshness
+    const message = { ...this.messages[randomIndex], timestamp: new Date().toISOString() };
+    return message;
   }
 }
